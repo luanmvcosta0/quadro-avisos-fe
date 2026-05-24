cbb692c8e5da7972426e5d33c23d274dd8cb388a
luanmvcosta0 <luanmvcosta@gmail.com>
2026-05-24 20:13:36 -0300

    feat: implement Header component in App

---
+++ src/notice_board/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="bg-amber-50  bg px-60 py-8 rounded-t-2xl">
+        Quadro de Avisos
+      </div>
+    </div>
+  );
+}
+
+export default Header;
