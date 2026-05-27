8f56507df4643687c012f43a9096b2738bce50f5
luanmvcosta0 <luanmvcosta@gmail.com>
2026-05-27 15:05:59 -0300

    feat: replace BellRing icon with BellIcon and update Header layout

--- src/notice_board/components/Header.tsx
+++ src/notice_board/components/Header.tsx
@@ -1,11 +1,18 @@
-import { BellRing } from "lucide-react";
+import { BellIcon } from "lucide-react";
 
 function Header() {
   return (
-    <div className="bg-slate-600 flex flex-col gap-6">
-      <div className="flex flex-row items-center ">
-        <BellRing />
-        <div className="text-2xl font-light">Quadro de Avisos</div>
+    <div className="bg-gray-200 w-screen h-screen flex flex-col items-center">
+      <div className="flex flex-row bg-white rounded-t-2xl p-6 w-150 shadow-md mt-10 justify-between">
+        <div className="flex flex-row gap-3">
+          <div className="bg-amber-100 rounded-md w-10 h-10 flex flex-col items-center justify-center">
+            <BellIcon size={28} />
+          </div>
+          <h1 className="text-2xl">Quadro de Avisos</h1>
+        </div>
+        <button className="bg-amber-500 text-white font-medium rounded-md w-27 h-8">
+          Novo Aviso
+        </button>
       </div>
     </div>
   );
